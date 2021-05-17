92dcd95e03f3706764b72f8d20b500604fce3097
Ankush Jain <anku.94@gmail.com>
2021-05-17 18:51:34 -0400

    parameterize the runner for compactor

--- tools/compactor_runner.cc
+++ tools/compactor_runner.cc
@@ -8,11 +8,39 @@
 #include <pdlfs-common/port.h>
 #include <stdio.h>
 
-int main() {
-  printf("hello world - compactor\n");
-  pdlfs::plfsio::RdbOptions options;
-  options.data_path = "/mnt/c/Users/Ankush/Repos/RDB";
+typedef pdlfs::plfsio::RdbOptions RdbOptions;
+
+void PrintHelp(const char* prog) { printf("Usage: %s -i <plfs_dir>", prog); }
+
+void ParseOptions(int argc, char* argv[], RdbOptions& options) {
+  extern char* optarg;
+  extern int optind;
+  int c;
+  while ((c = getopt(argc, argv, "i:")) != -1) {
+    switch (c) {
+      case 'i':
+        options.data_path = optarg;
+        break;
+      default:
+        PrintHelp(argv[0]);
+        exit(0);
+        break;
+    }
+  }
+
+  if (!options.env->FileExists(options.data_path.c_str())) {
+    logf(LOG_ERRO, "Dir not set, or set dir does not exist!");
+    PrintHelp(argv[0]);
+    exit(1);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  RdbOptions options;
   options.env = pdlfs::port::PosixGetDefaultEnv();
+  ParseOptions(argc, argv, options);
   pdlfs::plfsio::Compactor compactor(options);
-  compactor.Run();
+  pdlfs::Status s = compactor.Run();
+  logf(LOG_INFO, "Return Status: %s\n", s.ToString().c_str());
+  return 0;
 }
